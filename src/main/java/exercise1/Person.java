13ce781c4463176630e768eedab5646d53db365e
mauro-03 <mauro-03@hotmail.be>
2017-11-13 20:42:53 +0100

    .

--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,18 @@
 package exercise1;
 
 class Person {
+	private String name;
+	private int age;
+
+	public Person(String name, int age)
+	{
+		this.age=age;
+		this.name=name;
+	}
+
+	public String getName() { return this.name; }
+	public int getAge() { return this.age; }
+
+
+
 }
